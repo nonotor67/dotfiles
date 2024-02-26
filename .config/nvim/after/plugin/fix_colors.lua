0694ed944806d202d8b417d9f87678132dec197d
Noah CONTAL <noah.contal@epita.fr>
2024-02-26 04:22:27 -0500

    rm old dotfiles

--- .config/nvim/after/plugin/fix_colors.lua
+++
@@ -1,7 +0,0 @@
--- vim.api.nvim_set_hl(0, "@field", {})
--- vim.api.nvim_set_hl(0, "@property", {})
--- vim.api.nvim_set_hl(0, "@variable", {})
-
-vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = vim.g.material_colorscheme_map.guides.gui })
-vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = vim.g.material_colorscheme_map.guides.gui })
-vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = vim.g.material_colorscheme_map.guides.gui })
--- .config/nvim/ftdetect/s.vim
+++
@@ -1,1 +0,0 @@
-au BufRead,BufNewFile *.s set filetype=asm
